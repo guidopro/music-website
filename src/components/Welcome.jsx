4e76d84e9616585d749fac29c53059520bd2ad45
guidopro <169035608+guidopro@users.noreply.github.com>
2025-05-19 17:57:24 +0100

    updates bio

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,25 +3,6 @@ import { Link } from "react-router";
 export default function Welcome() {
   return (
     <>
-      {/* <div className="relative min-h-screen bg-[url(/src/assets/cover_bg_2.jpg)] bg-cover bg-[center_top_-200px] bg-no-repeat md:bg-center">
-        <div className="absolute inset-0 z-0 border-2 bg-black/50"></div>
-        <div className="relative z-10 p-10">
-          <div className="relative mt-85 md:mt-150">
-            <p className="font-extralight text-gray-200">
-              Music Producer and Composer
-            </p>
-            <hr />
-            <p className="font-sans text-white">Hi I'm Guy.</p>
-            <br />
-            <p className="font-sans text-base/7 text-gray-50">
-              I'm a composer and music producer based in Manchester, UK, with a
-              deep passion for travel, languages, and food. I've released
-              electronic music under the name <i>Guido</i>, and I'm now focused
-              on writing for orchestral instruments.
-            </p>
-          </div>
-        </div>
-      </div> */}
       <div className="relative">
         <img
           src="images/cover_bg_2_cropped.jpg"
@@ -63,13 +44,13 @@ export default function Welcome() {
             <br />
             <br />
             My journey began in the vibrant dubstep scene of Bristol during the
-            late 2000s and early 2010s, a time and place that profoundly shaped
-            my sound. My inspiration is rooted in the evocative power of film
-            and video game music. I’m particularly drawn to compositions that
-            create strong emotional landscapes, with composers like Vangelis,
-            Nobuo Uematsu, Joe Hisaishi, and Hans Zimmer being major influences.
-            Their ability to craft immersive, emotionally charged soundscapes
-            has always resonated with me and heavily influences my work.
+            late 2000s and early 2010s. My inspiration is rooted in the
+            evocative power of film and video game music. I’m particularly drawn
+            to compositions that create strong emotional landscapes, with
+            composers like Vangelis, Nobuo Uematsu, Joe Hisaishi, and Hans
+            Zimmer being major influences. Their ability to craft immersive,
+            emotionally charged soundscapes has always resonated with me and
+            heavily influences my work.
           </p>
         </div>
 
