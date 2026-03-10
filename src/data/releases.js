118732b53d7aca25a05e17d9896b10ca36e8ab8e
guidopro <169035608+guidopro@users.noreply.github.com>
2026-03-10 12:15:45 +0000

    change import method of assets

--- src/data/releases.js
+++ src/data/releases.js
@@ -1,76 +1,87 @@
+import softTouch from "/src/assets/artwork/JOY010-ARTWORK.png";
+import highTide from "/src/assets/artwork/high-tide-artwork.png";
+import stateOfFlux from "/src/assets/artwork/state-of-flux.jpg";
+import seeds from "/src/assets/artwork/seeds-artwork.jpg";
+import blockwork from "/src/assets/artwork/blockwork.jpeg";
+import endPoint from "/src/assets/artwork/endpoint.jpeg";
+import unknown from "/src/assets/artwork/unknown.jpeg";
+import onward from "/src/assets/artwork/JOY004-Artwork.png";
+import moment from "/src/assets/artwork/moment.jpeg";
+import flow from "/src/assets/artwork/flow.jpeg";
+import vesselDogs from "/src/assets/artwork/joy001.jpeg";
+import mofj from "/src/assets/artwork/MOFJ.jpeg";
+import anidea from "/src/assets/artwork/anidea.jpeg";
+
 const releases = [
   {
     title: "Soft Touch",
-    artwork: "/src/assets/artwork/JOY010-ARTWORK.png",
+    artwork: softTouch,
     embed:
       "https://bandcamp.com/EmbeddedPlayer/track=256926292/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/",
   },
   {
     title: "High Tide",
-    artwork: "/src/assets/artwork/high-tide-artwork.png",
+    artwork: highTide,
     embed:
       "https://bandcamp.com/EmbeddedPlayer/track=256926292/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/",
   },
   {
     title: "State of Flux",
-    artwork: "/src/assets/artwork/state-of-flux.jpg",
+    artwork: stateOfFlux,
     embed:
       "https://bandcamp.com/EmbeddedPlayer/album=680071508/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/",
   },
   {
     title: "Seeds",
-    artwork: "/src/assets/artwork/seeds-artwork.jpg",
+    artwork: seeds,
     embed:
       "https://bandcamp.com/EmbeddedPlayer/album=182900996/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/",
   },
   {
     title: "Blockwork",
-    artwork: "/src/assets/artwork/blockwork.jpeg",
+    artwork: blockwork,
     embed: "",
   },
   {
     title: "End Point",
-    artwork: "/src/assets/artwork/endpoint.jpeg",
+    artwork: endPoint,
     embed: "",
   },
   {
     title: "Unknown",
-    artwork: "/src/assets/artwork/unknown.jpeg",
+    artwork: unknown,
     embed: "",
   },
   {
     title: "Onward",
-    artwork: "/src/assets/artwork/JOY004-Artwork.png",
+    artwork: onward,
     embed: "",
   },
   {
     title: "Moment (feat. Emma-Lou)",
-    artwork: "/src/assets/artwork/moment.jpeg",
+    artwork: moment,
     embed: "",
   },
   {
     title: "Flow / Africa EP",
-    artwork: "/src/assets/artwork/flow.jpeg",
+    artwork: flow,
     embed: "",
   },
   {
     title: "Vessel Dogs / Micro X EP",
-    artwork: "/src/assets/artwork/joy001.jpeg",
+    artwork: vesselDogs,
     embed: "",
   },
   {
     title: "Moods Of Future Joy",
-    artwork: "/src/assets/artwork/MOFJ.jpeg",
+    artwork: mofj,
     embed: "",
   },
   {
     title: "Anidea",
-    artwork: "/src/assets/artwork/anidea.jpeg",
-    embed: "",
-  },
-  {
-    title: "Anidea",
-    artwork: "/src/assets/artwork/anidea.jpeg",
+    artwork: anidea,
     embed: "",
   },
 ];
+
+export default releases;
