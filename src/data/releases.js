11045759122eb11b51105438e94a2e0292f387ca
guidopro <169035608+guidopro@users.noreply.github.com>
2026-03-10 11:32:53 +0000

    add assets and release data

---
+++ src/data/releases.js
@@ -0,0 +1,76 @@
+const releases = [
+  {
+    title: "Soft Touch",
+    artwork: "/src/assets/artwork/JOY010-ARTWORK.png",
+    embed:
+      "https://bandcamp.com/EmbeddedPlayer/track=256926292/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/",
+  },
+  {
+    title: "High Tide",
+    artwork: "/src/assets/artwork/high-tide-artwork.png",
+    embed:
+      "https://bandcamp.com/EmbeddedPlayer/track=256926292/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/",
+  },
+  {
+    title: "State of Flux",
+    artwork: "/src/assets/artwork/state-of-flux.jpg",
+    embed:
+      "https://bandcamp.com/EmbeddedPlayer/album=680071508/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/",
+  },
+  {
+    title: "Seeds",
+    artwork: "/src/assets/artwork/seeds-artwork.jpg",
+    embed:
+      "https://bandcamp.com/EmbeddedPlayer/album=182900996/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/",
+  },
+  {
+    title: "Blockwork",
+    artwork: "/src/assets/artwork/blockwork.jpeg",
+    embed: "",
+  },
+  {
+    title: "End Point",
+    artwork: "/src/assets/artwork/endpoint.jpeg",
+    embed: "",
+  },
+  {
+    title: "Unknown",
+    artwork: "/src/assets/artwork/unknown.jpeg",
+    embed: "",
+  },
+  {
+    title: "Onward",
+    artwork: "/src/assets/artwork/JOY004-Artwork.png",
+    embed: "",
+  },
+  {
+    title: "Moment (feat. Emma-Lou)",
+    artwork: "/src/assets/artwork/moment.jpeg",
+    embed: "",
+  },
+  {
+    title: "Flow / Africa EP",
+    artwork: "/src/assets/artwork/flow.jpeg",
+    embed: "",
+  },
+  {
+    title: "Vessel Dogs / Micro X EP",
+    artwork: "/src/assets/artwork/joy001.jpeg",
+    embed: "",
+  },
+  {
+    title: "Moods Of Future Joy",
+    artwork: "/src/assets/artwork/MOFJ.jpeg",
+    embed: "",
+  },
+  {
+    title: "Anidea",
+    artwork: "/src/assets/artwork/anidea.jpeg",
+    embed: "",
+  },
+  {
+    title: "Anidea",
+    artwork: "/src/assets/artwork/anidea.jpeg",
+    embed: "",
+  },
+];
